6e1e5a82c1d6b79e19e524d2c4d70a60ea9cfba5
Shepico <Shepico@gmail.com>
2018-08-28 11:44:50 +0400

    Создана структура пакетов create interface directory and operation

---
+++ src/ru/shepico/directories/Idirectory.java
@@ -0,0 +1,10 @@
+package ru.shepico.directories;
+
+public interface Idirectory {
+
+    boolean addElement ();
+    boolean removeElement();
+    boolean changeElement();
+    boolean toArchiveElement();
+    boolean fromArchiveElemant();
+}
---
+++ src/ru/shepico/operations/Ioperation.java
@@ -0,0 +1,7 @@
+package ru.shepico.operations;
+
+public interface Ioperation {
+    boolean addOperation();
+    boolean removeOpearation();
+    boolean changeOperation();
+}
